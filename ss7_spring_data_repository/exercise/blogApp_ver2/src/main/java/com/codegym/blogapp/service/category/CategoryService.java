07d0c6d29aaf17532d03e073734b004fdda62154
HoaiNam2312 <namhoai2312@gmail.com>
2023-02-20 18:10:14 +0700

    add

---
+++ ss7_spring_data_repository/exercise/blogApp_ver2/src/main/java/com/codegym/blogapp/service/category/CategoryService.java
@@ -0,0 +1,34 @@
+package com.codegym.blogapp.service.category;
+
+import com.codegym.blogapp.model.Category;
+import com.codegym.blogapp.repository.ICategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService implements ICategoryService{
+    @Autowired
+    ICategoryRepository iCategoryRepository;
+
+    @Override
+    public List<Category> findAll() {
+        return iCategoryRepository.findAll();
+    }
+
+    @Override
+    public void save(Category category) {
+        iCategoryRepository.save(category);
+    }
+
+    @Override
+    public Category findById(int id) {
+        return iCategoryRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void remove(int id) {
+        iCategoryRepository.deleteById(id);
+    }
+}
